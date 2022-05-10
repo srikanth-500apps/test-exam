fd16ab10ae1369989fccf0ca78373630a14de7d7
srikanth-500apps <102018256+srikanth-500apps@users.noreply.github.com>
2022-05-10 17:08:58 +0530

    Update and rename README.md to 1st question.vue

---
+++ 1st question.vue
@@ -0,0 +1,19 @@
+# test-exam
+<template>
+  <div>
+    <b-table :items=items :fields=fields></b-table>
+    <div>
+  </template>
+    <script>
+      export default{
+      name:TesT,
+      data(){
+      return{[{pen,10,category]},
+      fields:[{PoductName, price, product category}]
+      items:{pen,10,category],
+      {phone, 10000, digital]
+      }
+      }
+      </cript>
+    <style>
+    </style>
